66ee768252fc47c0afe18b31e7335d53aa054900
nrietze <nils.rietze@uzh.ch>
2025-01-24 14:00:15 +0100

    adjust output patch for 23it HLS_SuPER

--- code/data_download/hls_dld_s3it.sh
+++ code/data_download/hls_dld_s3it.sh
@@ -12,7 +12,9 @@ source activate lpdaac_vitals
 echo 'HLS download starting.'
 date        ## Prints the system date
 
-python code/data_download/hls_downloader.py
+#python code/data_download/hls_downloader.py
+python code/data_download/HLS_SuPER.py -roi '144.4001779629389, 71.24588582272926,146.16765743760948, 71.6168891179392' -dir /home/nrietz/data/raster/hls/ -start 2020-07-01 -end 2020-07-05 -bands RED,GREEN,BLUE,NIR1,SWIR1,SWIR2,FMASK -qf True
+
 
 --output=home/$USER/scratch/logs/job_%j.out
 
