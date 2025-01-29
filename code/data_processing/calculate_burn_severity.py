5b6cb1a86b2d675ce0c049d4028248845c76f102
nrietze <nils.rietze@uzh.ch>
2025-01-29 18:46:49 +0100

    implemented advanced data filtering for HLS data post fire

--- code/data_processing/calculate_burn_severity.py
+++ code/data_processing/calculate_burn_severity.py
@@ -1,17 +1,21 @@
 # %%
 import os
 import multiprocessing
+import numpy as np
 import geopandas as gpd
 from osgeo import gdal
 import xarray as xr
 import rioxarray as rxr
 import rasterio as rio
+from rasterio.mask import mask
+from shapely.geometry import mapping
 import datetime
 from joblib import Parallel, delayed
 
 os.chdir("code/data_processing")
 import HLS_preprocessing as hlsPrep
 os.chdir("../..")
+
 # %% 0. Configure functions
 def get_raster_metadata(filepath):
     """Extract metadata for cloud and spatial coverage from a raster file."""
@@ -22,11 +26,135 @@ def get_raster_metadata(filepath):
         spatial_coverage = float(tags.get("spatial_coverage", 0))  # default to 0 if missing
         return cloud_coverage, spatial_coverage
 
-def filter_granules(granules, 
+def replace_string_in_filename(hls_filename, string):
+    """Replace the second last part of the filename with a string."""
+    out_string = f"{hls_filename.split('v2.0')[0]}v2.0_{string}.tif"
+    
+    return out_string
+
+def calculate_clear_pixel_percentage(files:list,
+                                     hls_processed_path:str,
+                                     polygon:gpd.GeoDataFrame,
+                                     bit_nums = [0,1,2,3,4]):
+    """Calculate percentage of non-NaN, non-water pixels in the polygon."""
+    # Band name pairs
+    if 'HLS.S30' in files[0]:
+        hls_band_dict = {
+            "COASTAL-AEROSOL": "B01",
+            "BLUE": "B02",
+            "GREEN": "B03",
+            "RED": "B04",
+            "RED-EDGE1": "B05",
+            "RED-EDGE2": "B06",
+            "RED-EDGE3": "B07",
+            "NIR-Broad": "B08",
+            "NIR1": "B8A",
+            "WATER-VAPOR": "B09",
+            "CIRRUS": "B10",
+            "SWIR1": "B11",
+            "SWIR2": "B12",
+            "FMASK": "Fmask",
+            "VZA": "VZA",
+            "VAA": "VAA",
+            "SZA": "SZA",
+            "SAA": "SAA",
+        }
+    else:
+        hls_band_dict = {
+            "COASTAL-AEROSOL": "B01",
+            "BLUE": "B02",
+            "GREEN": "B03",
+            "RED": "B04",
+            "NIR1": "B05",
+            "SWIR1": "B06",
+            "SWIR2": "B07",
+            "CIRRUS": "B09",
+            "TIR1": "B10",
+            "TIR2": "B11",
+            "FMASK": "Fmask",
+            "VZA": "VZA",
+            "VAA": "VAA",
+            "SZA": "SZA",
+            "SAA": "SAA",
+        }
+    
+    image = hlsPrep.load_rasters(files, "RED",
+                                 band_dict = hls_band_dict,
+                                 region=polygon,
+                                 chunk_size=dict(band=1, x=3600, y=3600))
+    
+    
+    # sample_file = [file for file in files if "B03" in file][0]
+    
+    # # Load the sample raster
+    # with rio.open(sample_file) as src:
+    #     polygon_geom = polygon.to_crs(src.crs).geometry.iloc[0]
+        
+    #     # Crop to the polygon area
+    #     image, _ = mask(src, [polygon_geom], crop=True)
+    #     nodata_value = src.nodata if src.nodata is not None else np.nan
+    
+    # # Load corresponding Fmask
+    # fmask_file = sample_file.replace('B03', 'Fmask')
+    # with rio.open(fmask_file) as fmask_src:
+    #     fmask, _ = mask(fmask_src, [polygon_geom], crop=True)
+    
+    fmask = hlsPrep.load_rasters(files, "FMASK",
+                                 band_dict = hls_band_dict,
+                                 region=polygon,
+                                 chunk_size=dict(band=1, x=3600, y=3600))
+    
+    
+    # Load corresponding water mask
+    water_mask_file = os.path.join(hls_processed_path,
+                                    os.path.basename(
+                                        replace_string_in_filename(files[0], "watermask")))
+    
+    with rio.open(water_mask_file) as watermask_src:
+        polygon_geom = polygon.to_crs(watermask_src.crs).geometry.iloc[0]
+        water_mask, _ = mask(watermask_src, [mapping(polygon_geom)], crop=True)
+
+    # Convert Fmask to binary
+    fmask_valid = hlsPrep.create_quality_mask(fmask.data, bit_nums)
+    
+    # Flatten arrays for easier pixel counting
+    image_flat = image.data.flatten()
+    fmask_valid_flat = fmask_valid.flatten()
+    water_mask_flat = water_mask.flatten()
+
+    # Mask water and NaN values
+    valid_pixels = np.isfinite(image_flat) 
+    non_water_pixels = water_mask_flat == 0  # Pixels that are not water
+
+    # Combine valid and non-water conditions
+    clear_pixels = valid_pixels & non_water_pixels & (fmask_valid_flat == 1)
+
+    # Compute percentage of clear pixels
+    percentage_clear = (np.sum(clear_pixels) / np.sum(valid_pixels)) * 100 if np.sum(valid_pixels) > 0 else 0
+    
+    return percentage_clear
+
+def filter_granules(granules:list,
+                    polygon:gpd.GeoDataFrame,
+                    hls_processed_path:str,
                     UTM_TILEID: str,
                     MAX_CLOUD_COVER: int,
-                    MIN_TILE_COVER: int):
-    """Filter granules based on cloud and spatial coverage criteria."""
+                    MIN_TILE_COVER: int,
+                    MIN_VALID_COVER: int):
+    """
+    Function that filters HLS granules on cloud and tile coverage, and the percent coverage of valid data over the fire perimeter. 
+
+    Args:
+        granules (list): _description_
+        polygon (gpd.GeoDataFrame): _description_
+        UTM_TILEID (str): _description_
+        MAX_CLOUD_COVER (int): _description_
+        MIN_TILE_COVER (int): _description_
+        MIN_VALID_COVER (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
     filtered_granules = []
     
     # Filter granules first by UTM tile ID
@@ -35,15 +163,20 @@ def filter_granules(granules,
     ]
     
     for granule_files in utm_filtered_granules:
-        sample_file = granule_files[0]  
 
         # Extract metadata without loading the raster
-        cloud_coverage, spatial_coverage = get_raster_metadata(sample_file)
+        cloud_coverage, spatial_coverage = get_raster_metadata(granule_files[0])
+        
+        clear_percentage = calculate_clear_pixel_percentage(granule_files, 
+                                                            hls_processed_path,
+                                                            polygon)
         
         # Apply filtering conditions
-        if cloud_coverage <= MAX_CLOUD_COVER and spatial_coverage >= MIN_TILE_COVER:
+        if cloud_coverage <= MAX_CLOUD_COVER and \
+            spatial_coverage >= MIN_TILE_COVER and \
+                clear_percentage >= MIN_VALID_COVER:
             filtered_granules.append(granule_files)
-    
+
     return filtered_granules
 
 def time_index_from_filenames(files):
@@ -66,6 +199,7 @@ else:
     print("Please prepare and filter the VIIRS fire perimeters using\n \"0_preprocess_ancillary_data.py\" ")
 
 HLS_PARENT_PATH = "/home/nrietz/scratch/raster/hls/"
+PROCESSED_HLS_DIR = '/home/nrietz/data/raster/hls/'
 
 TEST_ID = 14211 # fire ID for part of the large fire scar
 
@@ -94,19 +228,33 @@ hls_granules_paths = hlsPrep.search_files_by_doy_range(
     end_date.strftime("%Y-%m-%dT%H:%M:%S"), 
     season_end.strftime("%Y-%m-%dT%H:%M:%S"))
 
-# Filter tiles for < 20 % cloud cover and > 20 % tile coverage
+# Filter tiles for < 20 % cloud cover, > 20 % tile coverage, and > 20% of valid data
+MIN_VALID_COVERAGE = 80
 MIN_TILE_COVERAGE = 20
 MAX_CLOUD_COVERAGE = 80
 
-print(f"Filtering out HLS granules with more than {MAX_CLOUD_COVERAGE} % cloud cover \n and less than {MIN_TILE_COVERAGE} % tile coverage.")
+print(f"Filtering out HLS granules with more than {MAX_CLOUD_COVERAGE} % cloud cover\
+     \n and less than {MIN_TILE_COVERAGE} % tile coverage.")
 
 filtered_hls_granules = filter_granules(hls_granules_paths,
+                                        polygon = perimeter,
+                                        hls_processed_path=PROCESSED_HLS_DIR,
                                         UTM_TILEID=utm_tileid,
-                                        MAX_CLOUD_COVER=MAX_CLOUD_COVERAGE,MIN_TILE_COVER=MIN_TILE_COVERAGE)
+                                        MAX_CLOUD_COVER=MAX_CLOUD_COVERAGE,MIN_TILE_COVER=MIN_TILE_COVERAGE,
+                                        MIN_VALID_COVER=MIN_VALID_COVERAGE)
 
 print(f"Found {len(filtered_hls_granules)} good granules post-fire in UTM tile {utm_tileid}.")
 
 # %% 3. Calculate NBR for the filtered granules
+# Create list of unprocessed HLS granules for which no NBR file exists
+granules_without_nbr = [
+    granule for granule in filtered_hls_granules
+    if not os.path.exists(replace_string_in_filename(granule[0],'NBR'))  
+]
+
+# Calculate NBR and GEMI for the granules without NBR data
+print("Start calculation of NBR and GEMI for HLS granules missing that data.")
+
 N_CORES = -1 # -1 = all are used, -2 all but one
 OVERWRITE_DATA = False
 OUT_FOLDER = '/data/nrietz/raster/hls/'
@@ -116,14 +264,12 @@ multiprocessing.set_start_method('spawn')
 
 Parallel(n_jobs=N_CORES, backend='loky')(
     delayed(hlsPrep.joblib_hls_preprocessing)(files,
-                                              ["GEMI","NBR"],
-                                              bit_nums,
-                                              OUT_FOLDER,OVERWRITE_DATA) for files in filtered_hls_granules)
-
+                                            ["GEMI","NBR"],
+                                            bit_nums,
+                                            OUT_FOLDER,OVERWRITE_DATA) for files in granules_without_nbr)
+print("NBR and GEMI processing complete.")
 
 # %% 4. Stack NBR imagery
-PROCESSED_HLS_DIR = '/home/nrietz/data/raster/hls/'
-
 # List NBR COGs
 nbr_files = [
     os.path.abspath(os.path.join(PROCESSED_HLS_DIR, o)) 
